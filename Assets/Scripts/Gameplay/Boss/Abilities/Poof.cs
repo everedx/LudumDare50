27431f8802d5fe207a01d475fcde319a9da5cdea
Pablo Martinez <crackbashridge@gmail.com>
2022-04-02 23:21:53 +0200

    Poof location

--- Assets/Scripts/Gameplay/Boss/Abilities/Poof.cs
+++ Assets/Scripts/Gameplay/Boss/Abilities/Poof.cs
@@ -7,7 +7,7 @@ public class Poof : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        var bossPos = GameObject.FindWithTag("Player").transform.position;
+        var bossPos = GameObject.FindWithTag("Player").transform.Find("PoofLocation").transform.position;
         transform.position = bossPos;
         Destroy(gameObject, 2);
     }
